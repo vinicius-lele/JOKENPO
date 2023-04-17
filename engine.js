a8d6ff5ff48a5e2dd9e09a4fefa73efc303e6767
vinicois lele <vinicius.a.lele@hotmail.com>
2023-04-16 23:06:37 -0300

    Estilização, placar e zerar jogo

--- engine.js
+++ engine.js
@@ -1,23 +1,70 @@
 let escolhaUsuario,comparar,resultado,escolhaIA,texto;
+let pontoIA = 0;
+let pontoUsuario = 0;
 let opcoes = ['pedra.jpg','papel.jpg','tesoura.jpg'];
 let img = document.querySelectorAll('.opcao');
 let meuH3 = document.querySelector('h3');
 const novaDiv = document.createElement('div');
 novaDiv.setAttribute('class', 'resultado');
 
-function mostraDivResultado(usuario, ia){
-    texto = `
-                <div class="card">
-                    <img src="${usuario}">
-                    Sua escolha
-                </div>
-                <div class="card">
-                    <img src="${ia}">
-                    Escolha da máquina
-                </div>
-               <h2>`+resultado+`</h2>`;
+function mostraDivResultado(usuario, ia, comparar){
+    switch(comparar){
+        case -2:
+        case 1:
+            texto = `
+            <div class="card derrota">
+                <img src="${usuario}">
+                Você
+            </div>
+            <div class="card vitoria">
+                <img src="${ia}">
+                Máquina
+            </div>
+           <span>`+resultado+`</span>`;
+            break;
+        case 0:
+            texto = `
+            <div class="card empate">
+                <img src="${usuario}">
+                Você
+            </div>
+            <div class="card empate"">
+                <img src="${ia}">
+                Máquina
+            </div>
+           <span>`+resultado+`</span>`;
+            break;
+        default:
+            texto = `
+            <div class="card vitoria">
+                <img src="${usuario}">
+                Você
+            </div>
+            <div class="card derrota">
+                <img src="${ia}">
+                Máquina
+            </div>
+           <span>`+resultado+`</span>`;
+    }
+    
+    
+    
+    // texto = `
+    //             <div class="card">
+    //                 <img src="${usuario}">
+    //                 Você
+    //             </div>
+    //             <div class="card">
+    //                 <img src="${ia}">
+    //                 Máquina
+    //             </div>
+    //            <span>`+resultado+`</span>`;
                 
                 novaDiv.innerHTML = texto;
+                document.querySelector(".placar").innerHTML = `Você <span>${pontoUsuario}</span> X <span>${pontoIA}</span> Máquina
+                <p class="zerar"><span>ZERAR PLACAR</span></p>`;
+                document.querySelector(".zerar").addEventListener("click", ()=>{location.reload()});
+                
 }
     
 img.forEach(img_choice => {
@@ -29,17 +76,20 @@ img.forEach(img_choice => {
             case -2:
             case 1:
                 resultado = 'vitória da MÁQUINA';
+                pontoIA++;
                 break;
             case 0:
                 resultado = 'EMPATE';
                 break;
             default:
                 resultado = 'VOCÊ venceu';
+                pontoUsuario++;
         }
         meuH3.appendChild(novaDiv);
-        mostraDivResultado(opcoes[escolhaUsuario], opcoes[escolhaIA])
+        mostraDivResultado(opcoes[escolhaUsuario], opcoes[escolhaIA],comparar)
+        console.log(pontoIA)
+        console.log(pontoUsuario)
     })
             
 });
-    
-        
+
